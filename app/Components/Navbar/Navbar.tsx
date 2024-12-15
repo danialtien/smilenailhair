8900945b80a64a6886c32f704c6ec90bf157000f
227373 - Nguyen Tien <tientung2029901@gmail.com>
2024-12-15 09:59:16 +0700

    first commit

---
+++ app/Components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Disclosure } from '@headlessui/react';
+
+
+interface NavigationItem {
+    name: string;
+    href: string;
+    current: boolean;
+}
+
+const navigation: NavigationItem[] = [
+    { name: 'Home', href: '/', current: true },
+    { name: 'Services', href: '/services', current: false },
+    { name: 'Booking Now', href: '/booking-now', current: false },
+    { name: 'Gallery', href: '/gallery', current: false },
+    { name: 'Contact', href: '/contact', current: false },
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+}
+
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+        <Disclosure as="nav" className="navbar web-header">
+            <>
+                {/* Web customer menu */}
+                {/* Logo */}
+                <div className="">
+                    <Image src="/images/Logo/SmileNailHairLogo.png" alt="Logo" width={36} height={36} />
+                    <Link href="/" className='text-2xl font-semibold text-black ml-4'>
+                        Smile Nail & Hair.
+                    </Link>
+                </div>
+
+                {/* Navigation */}
+                <div className="">
+                    <div className="">
+                        {navigation.map((item) => (
+                            <Link key={item.name}
+                                href={item.href}
+                                className={classNames(
+                                    item.current ? 'bg-black' : 'navlinks hover:opacity-100', 
+                                    'px-3 py-4 rounded-md text-lg'
+                                )}
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+
+                {/* Mobile customer menu */}
+
+            </>
+        </Disclosure>
+    )
+}
+
+export default Navbar;
---
+++ app/Components/Navbar/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { use, useEffect } from "react";
+import Navbar from "./Navbar";
+
+
+const Nabarin: React.FC = () => {
+
+    useEffect(() => {
+        const debounce = (fn: any) => {
+            let frame: number;
+
+            return (...params: any[]) => {
+                if (frame) {
+                    cancelAnimationFrame(frame);
+                }
+                frame = requestAnimationFrame(() => {
+                    fn(...params);
+                });
+            }
+        };
+
+        const storeScoll = () => {
+            document.documentElement.dataset.scroll = window.scrollY.toString();
+        };
+
+        document.addEventListener('scroll', debounce(storeScoll), { passive: true });
+
+        storeScoll();
+    }, [])
+
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+
+export default Nabarin;
