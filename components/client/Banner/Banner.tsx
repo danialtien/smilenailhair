64638feed2a81010363b23cce40454b40fe53b35
schoolsupertech <sagemode771@gmail.com>
2024-12-20 19:05:48 +0700

    Update modified files

--- components/client/Banner/Banner.tsx
+++ components/client/Banner/Banner.tsx
@@ -45,7 +45,7 @@ export default function Banner() {
                 <button className="text-xl w-full md:w-auto font-medium rounded-full py-5 px-6 bg-pink-200 lg:px-14 mr-6">
                   <Link href="#cook-section">Join now</Link>
                 </button>
-                <button className="flex border w-full md:w-auto mt-5 md:mt-0 border-pink-200 justify-center rounded-full text-xl font-medium items-center py-5 px-10 hover:text-white hover:bg-pink-200">
+                <button className="flex border w-full md:w-auto mt-5 md:mt-0 border-pink-200 justify-center rounded-full text-xl font-medium items-center py-5 px-10 hover:bg-pink-200">
                   <Link href="#about-section">Explore now</Link>
                 </button>
               </div>
