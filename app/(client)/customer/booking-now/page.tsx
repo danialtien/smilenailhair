ebbbccb5d05c311217fb80e219ae74b898adf0a4
schoolsupertech <sagemode771@gmail.com>
2024-12-22 12:13:23 +0700

    :star::sparkles: New update :up:

--- app/(client)/customer/booking-now/page.tsx
+++ app/(client)/customer/booking-now/page.tsx
@@ -13,16 +13,46 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  userName: z.string().min(2).max(50),
+});
 
 export default function BookingPage() {
+  // formSchema
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      userName: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center gap-5 mt-2">
       <h1 className="text-xl font-semibold text-pink-600">Reserve your spot</h1>
-      <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-        <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-          <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+      <ol className="flex items-center w-2/5 text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+        <li className="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+          <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
             <svg
-              class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
+              className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -30,64 +60,84 @@ export default function BookingPage() {
             >
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
-            Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+            Personal <span className="hidden sm:inline-flex sm:ms-2">Info</span>
           </span>
         </li>
-        <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-          <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-            <span class="me-2">2</span>
-            Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+        <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+          <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            <span className="me-2">2</span>
+            Account <span className="hidden sm:inline-flex sm:ms-2">Info</span>
           </span>
         </li>
-        <li class="flex items-center">
-          <span class="me-2">3</span>
+        <li className="flex items-center">
+          <span className="me-2">3</span>
           Confirmation
         </li>
       </ol>
-      <div>
-        <Card className="w-full p-5 bg-amber-50">
-          <CardTitle className="mt-2">Appointment</CardTitle>
-          <CardDescription>Working time: 08:00 - 20:00</CardDescription>
-          <CardContent className="p-5">
-            <form>
-              <p className="text-sm font-bold text-pink-600 border-b border-pink-600 mb-2">
-                Please enter some information below
-              </p>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col">
-                  <label htmlFor="name" className="font-semibold">
-                    Name
-                  </label>
-                  <input
-                    id="name"
-                    placeholder="Name of your project"
-                    className="bg-amber-100 border border-amber-600 rounded p-2"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="framework" className="font-semibold">
-                    Framework
-                  </label>
-                  <Select>
-                    <SelectTrigger
-                      id="framework"
-                      className="bg-amber-100 border border-amber-600 rounded p-2"
-                    >
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
+      <Card className="w-2/5 p-5 bg-amber-50">
+        <CardTitle className="mt-2">Appointment</CardTitle>
+        <CardDescription>Working time: 08:00 - 20:00</CardDescription>
+        <CardContent className="p-5">
+          <Form {...form}>
+            <FormDescription>Note</FormDescription>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="userName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
             </form>
-          </CardContent>
-        </Card>
-      </div>
+          </Form>
+          <form>
+            <p className="text-sm font-bold text-pink-600 border-b border-pink-600 mb-2">
+              Please enter some information below
+            </p>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="name" className="font-semibold">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  placeholder="Name of your project"
+                  className="bg-amber-100 border border-amber-600 rounded p-2"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="framework" className="font-semibold">
+                  Framework
+                </label>
+                <Select>
+                  <SelectTrigger
+                    id="framework"
+                    className="bg-amber-100 border border-amber-600 rounded p-2"
+                  >
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Next.js</SelectItem>
+                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                    <SelectItem value="astro">Astro</SelectItem>
+                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </main>
   );
 }
