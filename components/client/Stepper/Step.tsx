7799ddccd1f3c2e4d8c33f3641f14c9d3e0d5de1
schoolsupertech <sagemode771@gmail.com>
2025-01-03 20:31:35 +0700

    :arrow_up_small: A little changing

--- components/client/Stepper/Step.tsx
+++ components/client/Stepper/Step.tsx
@@ -14,20 +14,17 @@ export default function Step({ step, isActive = false }: StepProps) {
       <li
         className={`flex md:w-full items-center ${isActive && "text-pink-600"} after:w-full after:h-1 after:border-b after:border-base after:border-1 after:hidden sm:after:inline-block after:mx-4 dark:after:border-gray-700`}
       >
-        <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-          {/* <MdOfflinePin size={22} className="mr-1" /> */}
-          <span
-            className={`w-4 h-4 flex items-center justify-center me-2 rounded-full border-2 ${isActive && "bg-pink-100 border-pink-600"}`}
-          >
-            <span
-              className={`text-sm font-bold ${isActive && "text-pink-600"}`}
-            >
-              {step.number}
-            </span>
+        {/* <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500"> */}
+        {/* <MdOfflinePin size={22} className="mr-1" /> */}
+        <span
+          className={`w-6 h-6 flex items-center justify-center me-2 rounded-full border-2 ${isActive ? "bg-pink-100 border-pink-600" : "border-base"}`}
+        >
+          <span className={`text-sm font-bold ${isActive && "text-pink-600"}`}>
+            {step.number}
           </span>
-          {step.title + " "}
-          <span className="hidden sm:inline-flex sm:ms-2">Info</span>
         </span>
+        <span className="hidden sm:inline-flex sm:ms-2">{step.title} Info</span>
+        {/* </span> */}
       </li>
       {/* <li className="flex md:w-full items-center text-pink-600 after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"> */}
       {/*   <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500"> */}
--- components/client/Stepper/Stepper.tsx
+++ components/client/Stepper/Stepper.tsx
@@ -5,7 +5,7 @@ export default function Stepper() {
   const { currentStep, steps } = useMultistepFormContext();
 
   return (
-    <ol className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+    <ol className="flex items-center w-full text-sm font-medium text-center dark:text-gray-400 sm:text-base">
       {steps.map((step) => (
         <Step
           key={step.number}
